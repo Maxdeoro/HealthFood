21420445049a7ee2bd17ba7ed851649604642dc4
Maxdeoro <maxdeoro@gmail.com>
2023-01-01 17:18:59 +0300

    added modal window

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 "use strict";
 document.addEventListener('DOMContentLoaded', () => {
-
     //Tabs
         const tabs = document.querySelectorAll('.tabheader__item'),
               tabsContent = document.querySelectorAll('.tabcontent'),
@@ -9,7 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
     
         function hideTabContent() {
             tabsContent.forEach( (item) => {
-                // item.style.display = 'none';
                 item.classList.add('hide');
                 item.classList.remove('show', 'fade');
             });
@@ -20,7 +18,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     
         function showTabContent(i = 0) {
-            //  tabsContent[i].style.display = 'block';
             tabsContent[i].classList.add('show', 'fade');
             tabsContent[i].classList.remove('hide');
              tabs[i].classList.add('tabheader__item_active');
@@ -42,13 +39,61 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     
+    //Modal
+    
+        const modalTrigger = document.querySelectorAll('[data-modal]'),
+              modal = document.querySelector('.modal'),
+              modalCloseBtn = document.querySelector('[data-close]'),
+              modalDialog = modal.querySelector('.modal__dialog');
+    
+        function openModal() {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';  //отключает прокрутку страницы вниз
+            clearInterval(modalTimer);    //предотвращает автооткрытие окна после открытия окна пользователем
+        }
+        function closeModal() {
+            modal.classList.add('hide');
+            modal.classList.remove('show');
+            document.body.style.overflow = 'auto';    //включает прокрутку страницы
+        }
+    
+        modalTrigger.forEach((btn) => {
+            btn.addEventListener('click', openModal);
+        });
+    
+        modalCloseBtn.addEventListener('click', closeModal);
+    
+        modal.addEventListener('click', (e) => {         //закрывает окно при клике на подложку
+            if(e.target === modal && e.target !== modalDialog) {
+                closeModal();
+            }
+        }); 
+        
+        document.addEventListener('keydown', (e) => {
+            if(e.key === 'Escape' && modal.classList.contains('show')) {
+                closeModal();
+            }
+        });
+    
+        const modalTimer = setTimeout(openModal, 30000);    //show modal in 30 sec
+    
+        function showModalByScroll() {
+            if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+                openModal();
+                window.removeEventListener('scroll', showModalByScroll);  //предотвращает повторное открытие modal
+            }  //условие означает что страница проскроллена до низа и открывается modal
+        }
+    
+        window.addEventListener('scroll', showModalByScroll);
+    
+    
     //Timer
         const deadline = '2022-10-20';
     
         function getTimeRemaining(endtime) {
             const t = Date.parse(endtime) - Date.parse(new Date()), //количество миллисекунд до deadline
                   days = Math.floor(t / (1000 * 60 * 60 * 24)),  //кол-во суток оставшихся до deadline, в скобках кол-во миллисек. в сутках
-                //   hours =Math.floor(((t / (1000 * 60 * 60 * 24)) - days) * 24),
                   hours = Math.floor((t / (1000 * 60 * 60) % 24)),  //от общего кол-ва часов берём остаток от деления на 24
                   minutes = Math.floor((t / (1000 * 60) % 60)),
                   seconds = Math.floor((t / 1000) % 60);
